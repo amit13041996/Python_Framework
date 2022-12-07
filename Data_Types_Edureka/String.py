ed95fa87ce9145c6a803ca634712422d91902e37
amit13041996 <119165064+amit13041996@users.noreply.github.com>
2022-12-07 18:30:11 +0530

    Update String

--- Data_Types_Edureka/String.py
+++ Data_Types_Edureka/String.py
@@ -11,10 +11,24 @@ print (message)
 
 Program = "\"Pyhton\""  # Using Escape sequence double quotes("") for O/p.
 print (Program)
-'''
+
 # --------------------------------------------------------------------------------
 sent = 'Python Coding'
 
 print(len(sent))
 print(sent.index('C'))
 print(sent.replace('P','J'))
+print(sent.split())
+
+# ---------------We can combine strings and numbers by using the format() method! ------------------------------------------------------------
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+'''
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+# print(myorder.format(quantity, itemno, price))
+
+print(myorder.encode())
