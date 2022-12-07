c26d3dbfddb9494cff4026acbc2f92c772428e84
amit13041996 <119165064+amit13041996@users.noreply.github.com>
2022-12-07 13:11:29 +0530

    String first commit

---
+++ Data_Types_Edureka/String.py
@@ -0,0 +1,20 @@
+'''
+# Python accepts both single('') and double("") quotes for String. 
+first_name = "Amit" 
+last_name = 'Srivastava'
+
+full_name = first_name + " " + last_name
+print (full_name)
+
+message = "I don't know"  # Using single quotes('') for O/p.
+print (message)
+
+Program = "\"Pyhton\""  # Using Escape sequence double quotes("") for O/p.
+print (Program)
+'''
+# --------------------------------------------------------------------------------
+sent = 'Python Coding'
+
+print(len(sent))
+print(sent.index('C'))
+print(sent.replace('P','J'))
